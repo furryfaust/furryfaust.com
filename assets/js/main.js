1e61391914bd6324d8a1680077a36a2ff023bb68
Kevin <kevtehpro35@gmail.com>
2016-02-28 19:03:03 -0500

    Added basic layout and small bio

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    var expand     = document.querySelector(".expand");
+    var expandSize = expand.clientHeight;
+    expand.style.maxHeight = 0;
+    document.querySelector(".fa.fa-caret-down").onclick = function() {
+        if (this.className.indexOf("fa-caret-down") != -1) {
+            expand.style.maxHeight = expandSize;
+            this.className = "fa fa-caret-up";
+        } else if (this.className.indexOf("fa-caret-up") != -1) {
+            expand.style.maxHeight = 0;
+            this.className = "fa fa-caret-down";
+        }
+    };
+};
